ceb854ab2916f258a369865fcce132b0f54c3c06
SebastianHurtado98 <37885169+SebastianHurtado98@users.noreply.github.com>
2018-08-21 21:45:27 -0500

    Add files via upload

--- list.h
+++ list.h
@@ -37,4 +37,99 @@ class List {
 
         ~List();
 };
+
+List::List(){
+  head=NULL;
+  tail=NULL;
+
+}
+
+List:: int front(){
+  return head;
+}
+List:: int back(){
+  return tail;
+}
+
+List:: void push_front(T value){
+  if(head==NULL){
+    head= value;
+    tail= value;
+  }
+  else{
+    Node<T>* temp;
+    temp=head;
+    head=value;
+    value.next=temp;
+  }
+}
+
+List:: void push_back(T value){
+  if(head==NULL){
+    head= value;
+    tail= value;
+  }
+  else{
+    tail.next= value;
+    value.next=NULL;
+    tail= value;
+  }
+}
+List:: void pop_front(){
+
+  //borrar el puntero
+  head=head.next;
+
+}
+List::void pop_back(){
+  Node<T>* temp=head;
+  while(temp.next!=tail){
+    temp=temp.next;
+  }
+  temp.next= NULL;
+}
+
+List:: T get(int position){
+  Node<T>* temp=head;
+  for (int i=0; i<position; i++){
+    temp=temp.next;
+  }
+
+  return temp.data;
+}
+
+List::void concat(List<T> &other){
+  tail.next= other.head;
+  //que hago con el tail?
+}
+
+List:: bool empty(){
+  return NULL==head;
+}
+
+List:: int size(){
+  return nodes;
+}
+
+List::void print(){
+  Node<T>* temp=head;
+  while (temp!=NULL){
+    cout << temp.data << endl;
+    temp=temp.next;
+  }
+}
+
+List:: void print_reverse(Node<T>* head){
+  if(head.next==NULL){
+    cout << head.data;
+  }
+  else{
+    print_reverse(head.next);
+  }
+}
+
+List:: void print_reverse(){
+  print_reverse(head);
+}
+
 #endif
